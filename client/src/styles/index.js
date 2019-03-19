d93896cf1c9d237ef980b6822c8cc3ef063f2e65
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-19 16:36:24 -0400

    added styled components directory

---
+++ client/src/styles/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SubmitBtn = styled.input`
+    background-color: #000;
+    border: 1px solid black;
+
+    &:hover {
+        background-color: #fff;
+        color: #000;
+    }
+`;
